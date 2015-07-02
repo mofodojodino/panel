3b3fc52bef04ed6a771fa6cf07bdc44b87d2d022
Dean Tedesco <dino@tedesco.net.au>
2015-07-03 09:32:13 +1000

    Refactored CrudController to use the IoC container to resolve all required classes.

--- src/controllers/CrudController.php
+++ src/controllers/CrudController.php
@@ -8,102 +8,154 @@ namespace Serverfireteam\Panel;
  * and open the template in the editor.
  */
 
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Routing\Controller;
 
+/**
+ * Class CrudController
+ * @package Serverfireteam\Panel
+ */
 class CrudController extends Controller
 {
+    /**
+     * @var Application
+     */
+    protected $app;
+    /**
+     * @var
+     */
+    protected $grid;
+    /**
+     * @var array
+     */
+    protected $entity;
+    /**
+     * @var
+     */
+    protected $edit;
+    /**
+     * @var
+     */
+    protected $filter;
 
-    
-    public $grid;
-    public $entity;
-    public $set;
-    public $edit;
-    public $filter;
-    protected $lang;
+    /**
+     * @param Application $app
+     */
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
 
-    public function __construct(\Lang $lang)
-    {         
-       // $this->entity = $params['entity'];
-        $route = \App::make('route');
-        $this->lang = $lang;
-        $this->route = $route;
-        $routeParamters = $route::current()->parameters();      
-        $this->setEntity($routeParamters['entity']);                      
+        if (self::$router) {
+            $params       = self::$router->current()->parameters();
+            $this->entity = $params['entity'];
+        } else {
+            $this->entity = [];
+        }
     }
 
     /**
-    * @param string $entity name of the entity
-    */
+     * @param string $entity name of the entity
+     */
     public function all($entity)
-    {                                                          
-        //$this->addStylesToGrid();                   
+    {
     }
-    
+
     /**
-    * @param string $entity name of the entity
-    */
+     * @param string $entity name of the entity
+     */
     public function edit($entity)
     {
-        
     }
 
 
-    public function getEntity(){
+    /**
+     * @return array
+     */
+    public function getEntity()
+    {
         return $this->entity;
     }
 
-     public function setEntity($entity){
+    /**
+     * @param $entity
+     */
+    public function setEntity($entity)
+    {
         $this->entity = $entity;
     }
-    
-    public function addStylesToGrid($orderByColumn = 'id',$paginateCount = 10)
+
+    /**
+     * @param string $orderByColumn
+     * @param int    $paginateCount
+     */
+    public function addStylesToGrid($orderByColumn = 'id', $paginateCount = 10)
     {
-        
         $this->grid->edit('edit', 'Edit', 'show|modify|delete');
 
         $this->grid->orderBy($orderByColumn, 'desc');
         $this->grid->paginate($paginateCount);
-        
     }
 
+    /**
+     * @return mixed
+     * @throws \Exception
+     */
     public function returnView()
     {
-        $configs = \Serverfireteam\Panel\Link::returnUrls();
-               
-        if ( !isset($configs) || $configs == null ){   
-            throw new \Exception('NO URL is set for yet');                                                      
-        } else if( !in_array($this->entity, $configs)){
-            throw new \Exception('This url is not set yet!');                                                                            
-        } else {        
-            return \View::make('panelViews::all', array(
-             'grid' 	      => $this->grid,
-             'filter' 	      => $this->filter,
-	     'current_entity' => $this->entity,
-	     'import_message' => (\Session::has('import_message')) ? \Session::get('import_message') : ''
-            ));   
-        }                      
+        $configs = Link::returnUrls();
+
+        if (!isset($configs) || $configs == null) {
+            throw new \Exception('NO URL is set for yet');
+        } else {
+            if (!in_array($this->entity, $configs)) {
+                throw new \Exception('This url is not set yet!');
+            } else {
+                return $this->app->make('view')->make(
+                    'panelViews::all',
+                    [
+                        'grid'           => $this->grid,
+                        'filter'         => $this->filter,
+                        'current_entity' => $this->entity,
+                        'import_message' => ($this->app->make('session')->has('import_message')) ? $this->app->make(
+                            'session'
+                        )->get('import_message') : '',
+                    ]
+                );
+            }
+        }
     }
-    
+
+    /**
+     * @return mixed
+     * @throws \Exception
+     */
     public function returnEditView()
     {
-        $configs = \Serverfireteam\Panel\Link::returnUrls();
-                
-        if ( !isset($configs) || $configs == null ){   
-            throw new \Exception('NO URL is set for yet');                                                      
-        } else if( !in_array($this->entity, $configs)){
-            throw new \Exception('This url is set yet !');                                                                            
-        }  else {        
-           return \View::make('panelViews::edit', array(
-             'edit' => $this->edit
-            )); 
-        }           
+        $configs = Link::returnUrls();
+
+        if (!isset($configs) || $configs == null) {
+            throw new \Exception('NO URL is set for yet');
+        } else {
+            if (!in_array($this->entity, $configs)) {
+                throw new \Exception('This url is set yet !');
+            } else {
+                return $this->app->make('view')->make(
+                    'panelViews::edit',
+                    [
+                        'edit' => $this->edit,
+                    ]
+                );
+            }
+        }
     }
-    
-     public function finalizeFilter(){
-        $lang = \App::make('lang');
-        $this->filter->submit($this->lang->get('panel::fields.search'));
-        $this->filter->reset($this->lang->get('panel::fields.reset'));
+
+    /**
+     *
+     */
+    public function finalizeFilter()
+    {
+        $lang = $this->app->make('lang');
+        $this->filter->submit($lang->get('panel::fields.search'));
+        $this->filter->reset($lang->get('panel::fields.reset'));
     }
-    
-    
 }
